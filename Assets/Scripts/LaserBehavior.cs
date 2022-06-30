6d064e7b8675992b65b45befa6b57af301b35681
Miles Lee <Miles.gr.lee@gmail.com>
2022-06-29 23:12:30 -0500

    Fixed laser grabbing

--- Assets/Scripts/LaserBehavior.cs
+++ Assets/Scripts/LaserBehavior.cs
@@ -17,8 +17,10 @@ public class LaserBehavior : MonoBehaviour
 
     [SerializeField] private InputActionReference _leftInteract;
     [SerializeField] private InputActionReference _rightInteract;
-    private GameObject _rightPicked;
-    private GameObject _leftPicked;
+    private Transform _rightPicked;
+    private Transform _leftPicked;
+    [SerializeField] private Transform _leftHand;
+    [SerializeField] private Transform _rightHand;
 
     void Start()
     {
@@ -47,6 +49,34 @@ public class LaserBehavior : MonoBehaviour
         {
             StopRightLaser();
         }
+
+        if (_leftPicked != null) 
+        {
+            if (Vector3.Distance(_leftPicked.position, _leftHand.position) > 0.2f)
+            {
+                _leftPicked.GetComponent<Rigidbody>().useGravity = false;
+                _leftPicked.position = Vector3.Lerp(_leftPicked.position, _leftHand.position, 0.05f);
+            }
+            else 
+            {
+                _leftPicked.GetComponent<Rigidbody>().useGravity = true;
+                _leftPicked = null;
+            }
+        }
+
+        if (_rightPicked != null)
+        {
+            if (Vector3.Distance(_rightPicked.position, _rightHand.position) > 0.2f)
+            {
+                _rightPicked.GetComponent<Rigidbody>().useGravity = false;
+                _rightPicked.position = Vector3.Lerp(_rightPicked.position, _rightHand.position, 0.05f);
+            }
+            else
+            {
+                _rightPicked.GetComponent<Rigidbody>().useGravity = true;
+                _rightPicked = null;
+            }
+        }
     }
 
     void LeftLaser() 
@@ -63,7 +93,10 @@ public class LaserBehavior : MonoBehaviour
                 _leftObj = hit.collider.gameObject;
                 _leftObj.GetComponent<Outline>().enabled = true;
                 _leftObj.GetComponent<Outline>().OutlineColor = _leftColor;
-                
+                if (_leftInteract.action.ReadValue<float>() == 1) 
+                {
+                    _leftPicked = _leftObj.transform;
+                }
             }
             else 
             {
@@ -94,6 +127,10 @@ public class LaserBehavior : MonoBehaviour
                 _rightObj = hit.collider.gameObject;
                 _rightObj.GetComponent<Outline>().enabled = true;
                 _rightObj.GetComponent<Outline>().OutlineColor = _rightColor;
+                if (_rightInteract.action.ReadValue<float>() == 1)
+                {
+                    _rightPicked = _rightObj.transform;
+                }
             }
             else 
             {
